5063ae2b40afafc7049cd8ec52a6f2ea30092615
Bruno Santi Garcia <bruno.santi.98@outlook.com>
2020-10-17 15:15:52 -0300

    test: ensure Login will not render spinner and error on mount

---
+++ src/presentation/pages/Login/login.spec.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import Login from '.'
+describe('Login component', () => {
+  test('Should not render error message on mount', () => {
+    const { queryByTestId } = render(<Login/>)
+    expect(queryByTestId('error-msg')).toBeFalsy()
+  })
+
+  test('Should not render spinner on mount', () => {
+    const { queryByTestId } = render(<Login/>)
+    expect(queryByTestId('spinner')).toBeFalsy()
+  })
+})
