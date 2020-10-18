6b3ca10eab23f9c15eef3fc6a983445810d2629b
Bruno Santi Garcia <bruno.santi.98@outlook.com>
2020-10-18 12:52:41 -0300

    feat: ensure Login calls validation with correct email

--- src/presentation/components/Input/index.tsx
+++ src/presentation/components/Input/index.tsx
@@ -2,18 +2,30 @@ import React from 'react'
 
 import Styles from './styles.scss'
 
-interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
-  error: string
+type stateProps ={
+  state: object
+  setState: React.Dispatch<React.SetStateAction<object>>
 }
 
-const Input: React.FC<Props> = ({ error, name, ...rest }: Props) => {
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  error: string
+  state: stateProps
+}
+
+const Input: React.FC<Props> = ({ error, name, state, ...rest }: Props) => {
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false
+  }
+  const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
+    state.setState({
+      ...state.state,
+      [event.target.name]: event.target.value
+    })
   }
 
   return (
     <div className={Styles.inputWrap}>
-      <input name={name} {...rest} readOnly onFocus={enableInput} />
+      <input name={name} data-testid={name} {...rest} readOnly onFocus={enableInput} onChange={handleChange}/>
       <span title={error} className={Styles.status} data-testid={`${name}-status`}>🔴</span>
     </div>
   )
--- src/presentation/pages/Login/index.tsx
+++ src/presentation/pages/Login/index.tsx
@@ -1,25 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Spinner, LoginHeader, Footer } from '@/presentation/components'
 import Styles from './styles.scss'
 import Input from '@/presentation/components/Input'
-const Login: React.FC = () => {
-  const [state] = useState({
-    isLoading: false
+import { Validation } from '@/presentation/protocols/Validation'
+
+type Props = {
+  validation: Validation
+}
+
+const Login: React.FC<Props> = ({ validation }: Props) => {
+  const [state, setState] = useState({
+    isLoading: false,
+    email: ''
   })
   const [error] = useState({
     message: '',
     email: 'Campo obrigatório',
     password: 'Campo obrigatório'
   })
+
+  useEffect(() => {
+    validation.validate({ email: state.email })
+  }, [state.email])
+
   return (
     <div className={Styles.container}>
       <LoginHeader/>
       <form className={Styles.form} data-testid="form">
         <h2>Login</h2>
         {error.message && <span className={Styles.error} data-testid="error-msg">{error.message}</span>}
-        <Input type="email" name="email" placeholder="Digite seu e-mail" error={error.email}/>
-        <Input type="password" name="password" placeholder="Digite sua senha" error={error.password}/>
+        <Input type="email" name="email" state={{ state, setState }} placeholder="E-mail" error={error.email}/>
+        <Input type="password" name="password" state={{ state, setState }} placeholder="Senha" error={error.password}/>
         <button type="submit" disabled>
           {state.isLoading && <Spinner /> }
             Entrar
