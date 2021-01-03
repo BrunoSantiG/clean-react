a0d6073330869625904005332f35e2ac2936af9c
BrunoSantiG <bruno.santi.98@outlook.com>
2021-01-02 23:00:07 -0300

    refactor: create a api url factory

--- src/main/factories/useCases/authentication/remote-authentication-factory.ts
+++ src/main/factories/useCases/authentication/remote-authentication-factory.ts
@@ -2,8 +2,8 @@
 import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication'
 import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory'
 import { Authentication } from '@/domain/useCases'
+import { makeApiUrl } from '../../http/api-url-factory'
 
 export const makeRemoteAuthentication = (): Authentication => {
-  const url = 'http://fordevs.herokuapp.com/api/login'
-  return new RemoteAuthentication(url, makeAxiosHttpClient())
+  return new RemoteAuthentication(makeApiUrl('/login'), makeAxiosHttpClient())
 }
