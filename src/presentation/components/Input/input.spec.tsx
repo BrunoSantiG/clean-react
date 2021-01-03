c8eb9ee8e8f27ff363e65282c38522c36288798e
BrunoSantiG <bruno.santi.98@outlook.com>
2021-01-02 22:54:05 -0300

    test: ensure Input component change readOnly to false onFocus event

--- src/presentation/components/Input/input.spec.tsx
+++ src/presentation/components/Input/input.spec.tsx
@@ -1,20 +1,34 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { fireEvent, render, RenderResult } from '@testing-library/react'
+import faker from 'faker'
+
 import Input from '.'
 
-const makeSut = (): RenderResult => (render(
-  <Input
-    name='field'
-    error=""
-    state={{ state: {}, setState: () => {} }}
-  />
-))
+const makeSut = (fieldName: string): RenderResult => (
+  render(
+    <Input
+      name={fieldName}
+      error=""
+      state={{ state: {}, setState: () => {} }}
+    />
+  )
+)
 
 describe('Input component', () => {
   test('Should begin with readOnly ', () => {
-    const { getByTestId } = makeSut()
+    const field = faker.database.column()
+    const { getByTestId } = makeSut(field)
 
-    const input = getByTestId('field') as HTMLInputElement
+    const input = getByTestId(field) as HTMLInputElement
     expect(input.readOnly).toBe(true)
+  })
+
+  test('Should change readOnly to false onFocus', () => {
+    const field = faker.database.column()
+    const { getByTestId } = makeSut(field)
+
+    const input = getByTestId(field) as HTMLInputElement
+    fireEvent.focus(input)
+    expect(input.readOnly).toBe(false)
   })
 })
